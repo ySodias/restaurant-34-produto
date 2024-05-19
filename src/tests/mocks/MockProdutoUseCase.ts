4956d32f9201fb82059cd8ff3a433508b0490f9a
bruno wake <brunowake.bw@gmail.com>
2024-05-19 17:35:55 -0300

    fix: import

--- src/tests/mocks/MockProdutoUseCase.ts
+++ src/tests/mocks/MockProdutoUseCase.ts
@@ -1,6 +1,6 @@
 import { NovoProduto } from '@/entities/NovoProduto';
 import ProdutoRepository from '@/external/repositories/ProdutoRepository';
-import prisma from '../utils/jest.setup';
+import { prisma } from '../utils/jest.setup';
 import { IProdutoRepository } from '@/interfaces/repositories/IProdutoRepository';
 
 const mockProdutoRepository: IProdutoRepository = new ProdutoRepository(prisma);
