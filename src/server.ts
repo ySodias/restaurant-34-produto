b0c93912cb7231d7a6efeaf3cfe6b859e58c5997
ySodias <63486008+ySodias@users.noreply.github.com>
2024-07-09 09:45:46 -0300

    Inserindo Headers de segurança

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from 'express';
 import "dotenv/config";
 import http from "http";
 import bodyParser from "body-parser";
@@ -8,6 +8,8 @@ import swaggerUi from "swagger-ui-express";
 import { routes } from "./api";
 import { prisma } from "./external/database";
 import request from 'supertest';
+import helmet from "helmet";
+import crypto from 'crypto';
 
 const app = express();
 
@@ -15,7 +17,39 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(cors());
+app.use(helmet());
+app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    next();
+  });
+
+  
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.locals.nonce = crypto.randomBytes(16).toString('hex');
+    next();
+  });
+  
+app.use(
+helmet.contentSecurityPolicy({
+    directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", (req: any, res: any) => `'nonce-${res.locals.nonce}'`],
+    styleSrc: ["'self'", 'https://fonts.googleapis.com', (req: any, res: any) => `'nonce-${res.locals.nonce}'`],
+    fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+    imgSrc: ["'self'"],
+    connectSrc: ["'self'"],
+    objectSrc: ["'none'"],
+    upgradeInsecureRequests: [],
+    },
+})
+);
+
+app.use(cors({
+    origin: ['179.125.135.67', 'https://www.google.com/']
+}));
+
+app.disable('x-powered-by');
 
 new routes(app, prisma);
 
